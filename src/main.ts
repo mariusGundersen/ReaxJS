bc231263396d558ba9f27e77e1890984b91793d2
Marius Gundersen <me@mariusgundersen.net>
2017-04-16 23:13:45 +0200

    FIx for infering generic types
    This is a hacky fix until TypeScript supports infering some of many specified generic types. This way only P can be specified and E, I and R can be inferred.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import reaxConnectorFactory, {ActionMappings, ObservablesFactory, Dict, Component} from './reaxConnectorFactory';
 
+export default function connect<P>()
+  : <E, I, R>(
+    actionMappings : ActionMappings<E, I>,
+    observablesFactory : ObservablesFactory<P, R, I>,
+    Component : Component<E, P, R>)
+    => React.ComponentClass<P>;
+
 export default function connect<E extends Dict, I extends Dict, R extends Dict, P>(
   actionMappings : ActionMappings<E, I>,
   observablesFactory : ObservablesFactory<P, R, I>,
   Component : Component<E, P, R>)
 : React.ComponentClass<P>;
+
 export default function connect<E extends Dict, I extends Dict, R extends Dict, P>(
   actionMappings : ActionMappings<E, I>,
   observablesFactory : ObservablesFactory<P, R, I>)
 : (Component : Component<E, P, R>) => React.ComponentClass<P>;
+
 export default function connect<E extends Dict, I extends Dict, R extends Dict, P>(
-  actionMappings : ActionMappings<E, I>,
-  observablesFactory : ObservablesFactory<P, R, I>,
+  actionMappings? : ActionMappings<E, I>,
+  observablesFactory? : ObservablesFactory<P, R, I>,
   Component? : Component<E, P, R>){
+  if(actionMappings === undefined || observablesFactory === undefined){
+    return <E, I, R>(
+      actionMappings : ActionMappings<E, I>,
+      observablesFactory : ObservablesFactory<P, R, I>,
+      Component : Component<E, P, R>) => connect(actionMappings, observablesFactory, Component);
+  }
   if(Component === undefined){
     return (Component : Component<E, P, R>) => connect(actionMappings, observablesFactory, Component);
   }
