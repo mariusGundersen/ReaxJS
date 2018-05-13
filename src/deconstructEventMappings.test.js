795064390f11234d6177f405ae55b056236d67ab
Marius Gundersen <me@mariusgundersen.net>
2018-05-13 20:45:40 +0200

    Added another test

--- src/deconstructEventMappings.test.js
+++ src/deconstructEventMappings.test.js
@@ -8,3 +8,22 @@ test('not a function should throw', t => {
     'test': null
   }));
 });
+
+
+
+test('triggering an action once should affect both subscribers', t => {
+  t.plan(2);
+
+
+  const subject = new Rx.BehaviorSubject();
+  const mapped = subject
+    .do(x => console.log('do', x))
+    .map(x => x*2);
+
+  mapped.subscribe();
+  mapped.subscribe();
+
+  subject.next(1);
+
+
+});
